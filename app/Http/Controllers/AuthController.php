8ef613cbc5a727db2d920df6f43f0465d66035f4
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-20 00:23:29 +0200

    finished the API to let the user upload his profile picture

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+
 use App\Models\User;
 use App\Models\Picture;
 
