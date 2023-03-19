8976d1fd85c5067d9677f4f02656508d094cc74b
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-19 22:55:36 +0200

    added the picture model directory to controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use App\Models\Picture;
 
 class AuthController extends Controller
 {
