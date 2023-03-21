1ee687895be954756aabf481e0c36e6cdfd0d4fa
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-21 19:59:25 +0200

    added functions and fillable for Users _chat model

--- app/Models/Users_chat.php
+++ app/Models/Users_chat.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Users_chat extends Model
 {
     use HasFactory;
+
+    protected $table = 'users_chats';
+    
+    protected $fillable = [
+        'users_id1',
+        'users_id2',
+    ];
+
+    public function chattingUser()
+    {
+        return $this->belongsTo(User::class, 'users_id1');
+    }
+
+    public function blockedUser()
+    {
+        return $this->belongsTo(User::class, 'users_id2');
+    }
 }
